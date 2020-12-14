54139cccdd2461bb4ef6e85b63665c6f5768ba94
blueochild <blueochild@gmail.com>
2020-12-14 13:31:58 +0900

    add : model/User파일 생성

--- src/main/java/com/blueochild/model/User.java
+++ src/main/java/com/blueochild/model/User.java
@@ -1,4 +1,16 @@
 package com.blueochild.model;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int user_id;
 }
