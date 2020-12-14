f78318a8b687a2be492c0b5f2dd437ca0ae99af7
blueochild <blueochild@gmail.com>
2020-12-14 14:12:58 +0900

    feat : User class를 추가한다

--- src/main/java/com/blueochild/model/User.java
+++ src/main/java/com/blueochild/model/User.java
@@ -13,4 +13,28 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int user_id;
+
+    @Column(length = 40, nullable = false)
+    private String email;
+
+    @Column(length = 20, nullable = false)
+    private String name;
+
+    @Column(length = 12)
+    private String phone;
+
+    @Builder
+    public User(String email, String name, String phone){
+        this.email = email;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString(){
+        return String.format(
+                "User[user_id=%d, email ='%s', name='%s', phone='%s]']",
+                this.user_id, this.email, this.name, this.phone
+        );
+    }
 }
