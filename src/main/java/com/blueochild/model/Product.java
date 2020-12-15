9371215f091f78200e98d3b863bca30f9693fd0d
blueochild <blueochild@gmail.com>
2020-12-15 09:20:40 +0900

    feat : product jpa project를 추가한다

---
+++ src/main/java/com/blueochild/model/Product.java
@@ -0,0 +1,44 @@
+package com.blueochild.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int productId;
+
+    @Column(length = 80, nullable = false)
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column
+    private int ListPrice;
+
+    @Column
+    private int price;
+
+    @Builder
+    public Product(String name, String description, int listprice, int price){
+        this.name = name;
+        this.description = description;
+        this.ListPrice = listprice;
+        this.price = price;
+    }
+
+    @Override
+    public String toString(){
+        return String.format(
+                "Product[productId='%s', name='%s', description='%s', listPrice=%d, pridce=%d]",
+                this.productId, this.name,this.description, this.ListPrice, this.price
+        );
+    }
+}
